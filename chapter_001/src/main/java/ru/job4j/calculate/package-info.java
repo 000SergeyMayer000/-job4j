c8a5432a618b99f2670c9a077bcf4812b0b65c93
Sergey Mayer <samajor@mail.ru>
2019-03-22 00:17:33 +0300

    изменил заглавную Y в имени на y

--- chapter_001/src/main/java/ru/job4j/calculate/package-info.java
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -1,7 +1,7 @@
 /**
         **Класс Calculate выводит текстовое сообщение "Hello world" в консоль.
         **
-        **@author SergeY Mayer(samajor@mail.ru)
+        **@author Sergey Mayer(samajor@mail.ru)
         **@version $Id$
         **@since 0.1
         **/
