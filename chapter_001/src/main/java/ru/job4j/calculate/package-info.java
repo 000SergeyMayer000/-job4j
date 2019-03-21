5dd399580d7b9da4a38567fd5e498d308544e0a8
Sergey Mayer <samajor@mail.ru>
2019-03-22 00:09:30 +0300

    1.3. JavaDoc. Code convention[#120326]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+        **Класс Calculate выводит текстовое сообщение "Hello world" в консоль.
+        **
+        **@author SergeY Mayer(samajor@mail.ru)
+        **@version $Id$
+        **@since 0.1
+        **/
+        *package ru.job4j.calculate;
