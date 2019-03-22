6b413c6743b02890611488b5122fa6c458545139
Sergey Mayer <samajor@mail.ru>
2019-03-22 23:49:32 +0300

    1.3. JavaDoc. Code convention[#120326] изменения комментариев для JavaDoc

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,16 +1,13 @@
-
+package ru.job4j.calculate;
 
 /**
  * Класс Calculate выводит текстовое сообщение "Hello world" в консоль
  * @author Sergey Mayer
  */
-
-package ru.job4j.calculate;
-
 public class Calculate {
     /**
-     *  Main method.
-     * @param args args
+     *  Main method точка входа в программу.
+     * @param  args - args.
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
