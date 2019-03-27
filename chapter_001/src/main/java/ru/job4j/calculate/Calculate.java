3d7d2e5ef852e759d53c2df6d01a8e3ce2ddfade
Sergey Mayer <samajor@mail.ru>
2019-03-27 08:52:23 +0300

    add metod echo

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -12,4 +12,12 @@ public class Calculate {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
+    /**
+     * Method echo.
+     * @param name Sergey Mayer.
+     * @return Echo plus Sergey Mayer.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
