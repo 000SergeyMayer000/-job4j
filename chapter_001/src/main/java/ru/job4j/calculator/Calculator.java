9468826b530b105f793ad54a258121ad12df792c
Sergey Mayer <samajor@mail.ru>
2019-03-30 20:30:50 +0300

    3.1. Элементарный калькулятор.[#120316]add JavaDoc

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,19 +1,55 @@
 package ru.job4j.calculator;
 
+/**
+ * Класс Calculator выполняет сложение,
+ * вычитание, деление у умножение двух чисел
+ *
+ * @author Sergey Mayer
+ */
 public class Calculator {
+    /**
+     * Сложение
+     *
+     * @param first  первое значение
+     * @param second второе значение
+     * @return результат
+     */
     public double add(double first, double second) {
-        return first + second; // сумма first и second
+        return first + second;
     }
 
+    /**
+     * вычитание
+     *
+     * @param first  первое значение
+     * @param second второе знвачение
+     * @return результат
+     */
     public double subtract(double first, double second) {
-        return first - second; // разность  first и second
+        return first - second;
     }
+
+    /**
+     * деление
+     *
+     * @param first  первое значение
+     * @param second второе знвачение
+     * @return результат
+     */
 
     public double div(double first, double second) {
-        return first / second; // деление
+        return first / second;
     }
 
+    /**
+     * умножение
+     *
+     * @param first  первое значение
+     * @param second второе знвачение
+     * @return результат
+     */
+
     public double multiple(double first, double second) {
-        return first * second; // умножение
+        return first * second;
     }
 }
