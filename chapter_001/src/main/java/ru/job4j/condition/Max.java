b43b77348cd4b7ff266f4dfa9df2e8e020181635
Sergey Mayer <samajor@mail.ru>
2019-04-01 19:14:42 +0300

    4.2. Максимум из двух чисел[#120286]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,8 +1,14 @@
 package ru.job4j.condition;
 
+/**
+ * Класс Max возращает большее число из двух.
+ *
+ * @author Sergey Mayer
+ */
+
 public class Max {
     public int max(int left, int right) {
         int result = left;
-        return left>right?left:right;
+        return left > right ? left : right;
     }
 }
