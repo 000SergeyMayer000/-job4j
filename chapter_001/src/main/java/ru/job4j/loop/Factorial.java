d3be68a160e3d1a27355c4cabefc58ccd1e3953c
Sergey Mayer <samajor@mail.ru>
2019-04-05 12:51:27 +0300

    5.2. Создать программу, вычисляющую факториал.[#120295] add JavaDoc

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -1,5 +1,9 @@
 package ru.job4j.loop;
-
+/**
+ * Класс Factorial вычисляет значение факториала от заданного числа.
+ *
+ * @author Sergey Mayer
+ */
 public class Factorial {
     public int calc(int n) {
         int result = 1;
