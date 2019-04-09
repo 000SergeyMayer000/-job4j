9b0d5507341d26108dc0e05a805c8add80f5832a
Sergey Mayer <samajor@mail.ru>
2019-04-09 20:45:30 +0300

    6.4. Слово начинается с ...[#120303]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -3,24 +3,24 @@ package ru.job4j.array;
 
 /**
  * Класс Check проверяет все ли элементы массива   являются true или false
- * @version 2.0
+ * @version 1.0
  */
 
 public class Check {
     /**
      * @param data
      * @return
-     *
      */
     public boolean mono(boolean[] data) {
         boolean result = false;
         for (int index = 0; index < data.length - 1; index++) {
-            if (data[index] != data[index + 1]) {
-                result = false;
-                break;
-            }
+            if (data[index] == data[index + 1]) {
                 result = true;
+            } else {
+                result = false;
             }
+
+        }
         return result;
     }
 
