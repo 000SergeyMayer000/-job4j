c67585bad676d5f9f980dd7be5bed1bfdd668c41
Sergey Mayer <samajor@mail.ru>
2019-04-10 09:31:10 +0300

    Класс Check проверяет все ли элементы массива   являются true или false

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -1,9 +1,9 @@
-
 package ru.job4j.array;
 
 /**
  * Класс Check проверяет все ли элементы массива   являются true или false
- * @version 1.0
+ *
+ * @version 2.0
  */
 
 public class Check {
@@ -14,12 +14,11 @@ public class Check {
     public boolean mono(boolean[] data) {
         boolean result = false;
         for (int index = 0; index < data.length - 1; index++) {
-            if (data[index] == data[index + 1]) {
-                result = true;
-            } else {
+            if (data[index] != data[index + 1]) {
                 result = false;
+                break;
             }
-
+            result = true;
         }
         return result;
     }
