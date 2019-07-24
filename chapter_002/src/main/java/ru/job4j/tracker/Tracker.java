80e77a8e0c6a3d69d6d2033f117ef89edf829c44
Sergey Mayer <samajor@mail.ru>
2019-07-25 00:38:11 +0300

    create: Item.java, Tracker.java, TrackerTest.java

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,74 @@
 package ru.job4j.tracker;
 
-public class Tracker {
+import java.util.*;
 
+public class Tracker {
+    private Item[] items = new Item[100];
+    private int position = 0;
+    private static final Random RN = new Random();
+
+    public Item add(Item item) {
+        item.setId(String.valueOf(System.currentTimeMillis() + RN.nextInt()));
+        this.items[position++] = item;
+
+        return item;
+    }
+
+    public boolean replace(String id, Item item) {
+        boolean result = false;
+        for (int i = 0; i < items.length; i++) {
+            if (item.getId() == id) {
+                this.items[i] = item;
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public boolean delete(String id) {
+        boolean result = false;
+        Item item = null;
+        for (int i = 0; i < items.length; i++) {
+            if (item != null && item.getId().equals(id)) {
+                System.arraycopy(items, i + 1, items, i, items.length - i - 1);
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[position];
+        for (int index = 0; index < position; index++) {
+            result[index] = this.items[index];
+        }
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        int i = 0;
+        Item[] result = null;
+        for (Item item : items) {
+            if (item.getName() != null && item.getName().equals(key)) {
+                result[i] = item;
+                i++;
+            }
+        }
+        return result;
+
+    }
+
+    public Item findById(String id) {
+        Item result = null;
+        for (Item item : items) {
+            if (item != null && item.getId().equals(id)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
+    }
 }
+
