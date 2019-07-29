08e0122a54dc99052567d1b9594c8bfddf78ca4d
Sergey Mayer <samajor@mail.ru>
2019-07-29 23:38:48 +0300

    correction Tracker.java

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -8,18 +8,18 @@ public class Tracker {
     private static final Random RN = new Random();
 
     public Item add(Item item) {
-        item.setId(String.valueOf(System.currentTimeMillis() + RN.nextInt()));
+        item.setId(generateId());
         this.items[position++] = item;
-
         return item;
     }
 
     public boolean replace(String id, Item item) {
         boolean result = false;
-        for (int i = 0; i < items.length; i++) {
-            if (item.getId() == id) {
+        for (int i = 0; i < position; i++) {
+            if (item.getId().equals(id)) {
                 this.items[i] = item;
                 result = true;
+                item.setId(id);
                 break;
             }
         }
@@ -29,10 +29,11 @@ public class Tracker {
     public boolean delete(String id) {
         boolean result = false;
         Item item = null;
-        for (int i = 0; i < items.length; i++) {
+        for (int i = 0; i < position; i++) {
             if (item != null && item.getId().equals(id)) {
                 System.arraycopy(items, i + 1, items, i, items.length - i - 1);
                 result = true;
+                position--;
                 break;
             }
         }
@@ -40,35 +41,38 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] result = new Item[position];
-        for (int index = 0; index < position; index++) {
-            result[index] = this.items[index];
-        }
-        return result;
+        return Arrays.copyOf(this.items, this.position);
     }
 
     public Item[] findByName(String key) {
-        int i = 0;
+        Item item = null;
         Item[] result = null;
-        for (Item item : items) {
+        int number=position;
+        for (int i = 0; i < position; i++) {
             if (item.getName() != null && item.getName().equals(key)) {
                 result[i] = item;
                 i++;
+                number--;
             }
         }
-        return result;
+        return Arrays.copyOf(result, number);
 
     }
 
     public Item findById(String id) {
+        Item item=null;
         Item result = null;
-        for (Item item : items) {
+        for (int i = 0; i < position; i++) {
             if (item != null && item.getId().equals(id)) {
                 result = item;
                 break;
             }
         }
-        return result;
+        return  result;
+    }
+
+    public String generateId() {
+        return String.valueOf(System.currentTimeMillis() + RN.nextInt());
     }
 }
 
