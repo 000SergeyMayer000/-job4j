29d8d15142a9b398bd62af1c6ce54bc0732e0789
Sergey Mayer <samajor@mail.ru>
2019-09-19 23:27:38 +0300

    upcasting, downcasting

---
+++ chapter_002/src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class College {
+
+    Student student =new Freshman(); //upcasting - восходящее преобразование( преобрахование
+                                     // происходит автоматически, не явно
+    Object object = new Freshman();  //upcasting
+
+    Freshman freshman1 = (Freshman) student; //down casting - низходящее преобразование (необходимо
+                                             // явно перобразовывать.
+
+}
