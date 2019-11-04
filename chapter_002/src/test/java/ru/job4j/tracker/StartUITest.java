f358cb5714d5d739be76a65f79dc12185792baff
Sergey Mayer <samajor@mail.ru>
2019-11-05 00:30:14 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#120363]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -17,7 +17,7 @@ public class StartUITest {
                 new String[]{"0"}
         );
         StubAction action = new StubAction();
-        new StartUI111().init(input, new Tracker(), new UserAction[]{action});
+        new StartUI().init(input, new Tracker(), new UserAction[]{action});
         assertThat(action.isCall(), is(true));
     }
 
@@ -30,11 +30,35 @@ public class StartUITest {
                 new String[] {"0"}
         );
         StubAction action = new StubAction();
-        new StartUI111().init(input, new Tracker(), new UserAction[] { action });
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
         String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
                 .add("           Меню.")
                 .add("0. Stub action")
                 .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+    @Test
+    public void whenCheckOutput() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        Item item = new Item("name 1", "desc 1", 2610201914144L);
+        tracker.add(item);
+        AllShowAction act = new AllShowAction();
+        act.execute(new StubInput(new String[] {}), tracker);
+        StringBuilder expect = new StringBuilder();
+        expect.append("Заявка с именем " + item.getName());
+        expect.append(System.lineSeparator());
+        expect.append("ID заявки: " + item.getId());
+        expect.append(System.lineSeparator());
+        expect.append("Дата создания: " + item.getTime());
+        expect.append(System.lineSeparator());
+        expect.append("Описание: " + item.getDecs());
+        expect.append(System.lineSeparator());
+        expect.toString();
+
         assertThat(new String(out.toByteArray()), is(expect));
         System.setOut(def);
     }
