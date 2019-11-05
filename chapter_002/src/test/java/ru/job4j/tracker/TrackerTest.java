6e313ef82cb77f1d98fdc91e93d95b89330f85b4
Sergey Mayer <samajor@mail.ru>
2019-11-05 10:30:54 +0300

    change TrackerTest

--- chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -20,29 +20,20 @@ public class TrackerTest {
     public void whenReplaceNameThenReturnNewName() {
         Tracker tracker = new Tracker();
         Item previous = new Item("test1", "testDescription", 123L);
-        // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
         tracker.add(previous);
-        // Создаем новую заявку.
         Item next = new Item("test2", "testDescription2", 1234L);
-        // Проставляем старый id из previous, который был сгенерирован выше.
         next.setId(previous.getId());
-        // Обновляем заявку в трекере.
         tracker.replace(previous.getId(), next);
-        // Проверяем, что заявка с таким id имеет новые имя test2.
         assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
     }
 
     @Test
     public void whenDeleteItem() {
         Tracker tracker = new Tracker();
-        int positionstart = tracker.position;//состояние счетчика в момент когда заявок нет(position=0)
-        System.out.println(tracker.position);
         Item item1 = new Item("name1", "decs1", 54321L);
         tracker.add(item1);
-        System.out.println(tracker.position);
-        int position1 = tracker.position;// position=1
+        int counter = tracker.position;
         tracker.delete(item1.getId());
-        System.out.println(tracker.position);
-        assertThat(tracker.position, is(0));//после удаления заявки счетчик снова обнулится.
+        assertThat(tracker.position, is(counter - 1));
     }
 }
