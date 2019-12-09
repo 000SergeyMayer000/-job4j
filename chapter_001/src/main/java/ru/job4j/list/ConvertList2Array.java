87393ee7c1b65ec29525e0aa97c20ca74503b0be
000SergeyMayer000 <samajor@mail.ru>
2019-12-10 00:08:42 +0300

    replace ConvertList2Array.java

--- chapter_001/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_001/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -3,25 +3,20 @@ package ru.job4j.list;
 import java.util.List;
 
 public class ConvertList2Array {
-    public int[][] toArray(List<Integer> list, int rows) {
-        int cells ;
-        if (list.size() % rows == 0) {
-            cells = list.size() / rows;
-        } else {
-            cells = (list.size() / rows) + 1;
-        }
-        int[][] array = new int[rows][cells];
-        int counter = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cells; j++) {
-                if (list.size() > counter) {
-                    array[i][j] = list.get(counter++);
-                } else {
-                    array[i][j] = 0;
-                }
+    public static int[][] toArray(List<Integer> list, int cells) {
+        int groups = (int) Math.ceil((double) list.size() / cells);
+        int[][] array = new int[groups][cells];
+        int row = 0, cell = 0;
+        for (Integer num : list) {
+            if(cell!=cells){
+                array[row][cell] = num;
+                cell++;
+            }else{
+                row++;
+                cell=0;
+             array[row][cell++] = num;
             }
         }
         return array;
     }
 }
-
