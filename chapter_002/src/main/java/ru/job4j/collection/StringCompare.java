f3259d7e1eebed2fff868e58700f6ff5c6e91bdf
000SergeyMayer000 <samajor@mail.ru>
2019-12-28 03:05:56 +0300

    3. Компаратор для строк.[#120341] add StringCompare &StringCompareTest

---
+++ chapter_002/src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,43 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        List<Character> leftList = new ArrayList<>();
+        char[] leftArray = o1.toCharArray();
+        for (Character character : leftArray) {
+            leftList.add(character);
+        }
+        List<Character> rightList = new ArrayList<>();
+        char[] rightArray = o2.toCharArray();
+        for (Character character : rightArray) {
+            rightList.add(character);
+        }
+        int rsl = -1;
+        int index = Math.min(leftList.size(), rightList.size());
+        for (int i = 0; i < index; i++) {
+            if (Character.compare(leftList.get(i), rightList.get((i))) == 1 || (Integer.compare(leftList.get(i), rightList.get((i)))) == 1) {
+                rsl = 1;
+                break;
+            } else if (Character.compare(leftList.get(i), rightList.get((i))) == 0 || (Integer.compare(leftList.get(i), rightList.get((i)))) == 0) {
+                rsl = 0;
+            } else {
+                rsl = -1;
+                break;
+            }
+        }
+        if (rsl == 0) {
+            if (leftList.size() > rightList.size()) {
+                rsl = 1;
+            }  else if (leftList.size() < rightList.size()){
+                rsl = -1;
+            }
+        }
+        return rsl;
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/collection/StringCompareTest.java
@@ -0,0 +1,111 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class StringCompareTest {
+    @Test
+    public void whenStringsAreEqualThenZero() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanov"
+        );
+        assertThat(rst, is(0));
+    }
+
+    @Test
+    public void whenLeftLessThanRightResultShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanova"
+        );
+        assertThat(rst, lessThan(0));
+    }
+
+    @Test
+    public void whenLeftGreaterThanRightResultShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Ivanova"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+    @Test
+    public void secondCharOfLeftGreaterThanRightShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Patrov"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+    @Test
+    public void secondCharOfLeftLessThanRightShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Patrova",
+                "Petrov"
+        );
+        assertThat(rst, lessThan(0));
+    }
+
+    @Test
+    public void whenLeftLargeThanRightResultShouldBePositiv() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Ivanova",
+                "Ivanov"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+
+    @Test
+    public void whenStringsIntegerAreEqualThenZero() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "123456",
+                "123456"
+        );
+        assertThat(rst, is(0));
+    }
+
+    @Test
+    public void secondIntegerOfLeftLessThanRightShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "123456789",
+                "133456789"
+        );
+        assertThat(rst, lessThan(0));
+    }
+
+    @Test
+    public void whenLeftIntegerLessThanRightResultShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "12345",
+                "1234567"
+        );
+        assertThat(rst, lessThan(0));
+    }
+
+    @Test
+    public void whenLeftIntegerLargeThanRightResultShouldBePositiv() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "1234567",
+                "123456"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+}
