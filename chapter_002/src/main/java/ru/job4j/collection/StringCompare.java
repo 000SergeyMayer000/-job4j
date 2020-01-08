a4ea75fd30fb0ef8775fcee6514070e213f1bb5a
000SergeyMayer000 <samajor@mail.ru>
2020-01-08 21:14:12 +0300

    replace README.md

--- chapter_002/src/main/java/ru/job4j/collection/StringCompare.java
+++ chapter_002/src/main/java/ru/job4j/collection/StringCompare.java
@@ -8,36 +8,51 @@ import java.util.List;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        List<Character> leftList = new ArrayList<>();
-        char[] leftArray = o1.toCharArray();
-        for (Character character : leftArray) {
-            leftList.add(character);
-        }
-        List<Character> rightList = new ArrayList<>();
-        char[] rightArray = o2.toCharArray();
-        for (Character character : rightArray) {
-            rightList.add(character);
-        }
-        int rsl = -1;
-        int index = Math.min(leftList.size(), rightList.size());
-        for (int i = 0; i < index; i++) {
-            if (Character.compare(leftList.get(i), rightList.get((i))) == 1 || (Integer.compare(leftList.get(i), rightList.get((i)))) == 1) {
-                rsl = 1;
-                break;
-            } else if (Character.compare(leftList.get(i), rightList.get((i))) == 0 || (Integer.compare(leftList.get(i), rightList.get((i)))) == 0) {
-                rsl = 0;
-            } else {
-                rsl = -1;
-                break;
-            }
-        }
-        if (rsl == 0) {
-            if (leftList.size() > rightList.size()) {
-                rsl = 1;
-            } else if (leftList.size() < rightList.size()) {
-                rsl = -1;
-            }
-        }
-        return rsl;
+        return 0;
     }
 }
+//    @Override
+//    public int compare(String o1, String o2) {
+//        int rsl = 0;
+//        int a = Math.min(o1.length(), o2.length());
+//        for (int i = 0; i < a; i++) {
+//            rsl = Integer.compare((o1.charAt(i)), (o2.charAt(i)));
+//            if (rsl != 0) {
+//                break;
+//            } else if ((o1.length() > o2.length())){
+//                rsl = 1;
+//                break;
+//            }
+
+//
+//        List<Character> leftList = new ArrayList<>();
+//        char[] leftArray = o1.toCharArray();
+//        for (Character character : leftArray) {
+//            leftList.add(character);
+//        }
+//        List<Character> rightList = new ArrayList<>();
+//        char[] rightArray = o2.toCharArray();
+//        for (Character character : rightArray) {
+//            rightList.add(character);
+//        }
+//        int rsl = -1;
+//        int index = Math.min(leftList.size(), rightList.size());
+//        for (int i = 0; i < index; i++) {
+//            if (Character.compare(leftList.get(i), rightList.get((i))) == 1 || (Integer.compare(leftList.get(i), rightList.get((i)))) == 1) {
+//                rsl = 1;
+//                break;
+//            } else if (Character.compare(leftList.get(i), rightList.get((i))) == 0 || (Integer.compare(leftList.get(i), rightList.get((i)))) == 0) {
+//                rsl = 0;
+//            } else {
+//                rsl = -1;
+//                break;
+//            }
+//        }
+//        if (rsl == 0) {
+//            if (leftList.size() > rightList.size()) {
+//                rsl = 1;
+//            } else if (leftList.size() < rightList.size()) {
+//                rsl = -1;
+//            }
+//        }
+
--- chapter_002/src/main/java/ru/job4j/collection/Test.java
+++ chapter_002/src/main/java/ru/job4j/collection/Test.java
@@ -5,23 +5,39 @@ import java.util.*;
 public class Test {
 
     public static void main(String[] args) {
-        List<User> users = Arrays.asList(
-                new User("Ivan", 21),
-                new User("Anton", 21),
-                new User("Petr", 35),
-                new User("Oleg", 29)
-        );
-        Collections.sort(users, new SortByAgeAscendingSort());
-        System.out.println(users); // сортировка Age по ВОЗРОСТАНИЮ
-        Collections.sort(users, new SortByAgeDescendingSort());
-        System.out.println(users); // сортировка Age по УБЫВАНИЮ
-        Collections.sort(users, new SortByAgeAscendingSort());
-        System.out.println(users); // сортировка Age по ВОЗРОСТАНИЮ
-        Collections.sort(users, new SortByNameAscendingSort());
-        System.out.println(users); // сортировка Name по ВОЗРОСТАНИЮ
-        Collections.sort(users, new SortByNameDescendingSort());
-        System.out.println(users); // сортировка Name по УБЫВАНИЮ
-        Collections.sort(users, new SortByAgeAscendingSort().thenComparing(new SortByNameDescendingSort())); // комбинированный компаратор
-        System.out.println(users);
+
+        String str1 = "Ivanov";
+        System.out.println(str1.charAt(5));
+//        String str2 = "Ivanova";
+//        int rslInteger = -1;
+//        int a = Math.min(str1.length(), str2.length());
+//        for (int i = 0; i < a; i++) {
+//            rslInteger = Integer.compare((str1.charAt(i)), (str2.charAt(i)));
+//---------------------------------------------------------------------------------
+            //
+            //  int rslCharacter = Character.compare((str1.charAt(i)), (str2.charAt(i)));
+//        List<User> users = Arrays.asList(
+//                new User("Ivan", 21),
+//                new User("Anton", 21),
+//                new User("Petr", 35),
+//                new User("Oleg", 29)
+//        );
+//        Collections.sort(users, new SortByAgeAscendingSort());
+//        System.out.println(users); // сортировка Age по ВОЗРОСТАНИЮ
+//        Collections.sort(users, new SortByAgeDescendingSort());
+//        System.out.println(users); // сортировка Age по УБЫВАНИЮ
+//        Collections.sort(users, new SortByAgeAscendingSort());
+//        System.out.println(users); // сортировка Age по ВОЗРОСТАНИЮ
+//        Collections.sort(users, new SortByNameAscendingSort());
+//        System.out.println(users); // сортировка Name по ВОЗРОСТАНИЮ
+//        Collections.sort(users, new SortByNameDescendingSort());
+//        System.out.println(users); // сортировка Name по УБЫВАНИЮ
+//        Collections.sort(users, new SortByAgeAscendingSort().thenComparing(new SortByNameDescendingSort())); // комбинированный компаратор
+//        System.out.println(users);
+
+
+        }
+      //  System.out.println(rslInteger);
     }
-}
+
+
