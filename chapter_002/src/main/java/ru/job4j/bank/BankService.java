7aa12b3ef81ec0a8b2533a3454eb367a4e8aaee9
000SergeyMayer000 <samajor@mail.ru>
2020-01-10 22:38:47 +0300

    3. Банковские переводы. [#10038] add User.java & Account.java & BankService.java BankServiceTest.java

---
+++ chapter_002/src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,94 @@
+package ru.job4j.bank;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        List<Account> accounts = new ArrayList<>();
+        if (users.containsKey(user)) {
+            System.out.println("Such a client exists");
+        } else {
+            users.put(user, accounts);
+        }
+    }
+
+    public void addAccount(String passport, Account account) {
+        BankService bank = new BankService();
+        User user = bank.findByPassport(passport);
+        List<Account> accounts = users.get(user);
+//        int index = accounts.indexOf(account);
+//        if (index >= 0) {
+//            System.out.println("Such an account exists");
+//
+//        } else {
+        accounts.add(account);
+    }
+//    }
+
+    public User findByPassport(String passport) {
+        BankService bank = new BankService();
+        User rsl = null;
+        for (Map.Entry<User, List<Account>> userListEntry : bank.users.entrySet()) {
+            if (userListEntry.getKey().getPassport().equals(passport)) {
+                rsl = userListEntry.getKey();
+                break;
+            } else {
+                System.out.println("There is no such client");
+            }
+        }
+        return rsl;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        BankService bank = new BankService();
+        User user = bank.findByPassport(passport);
+        List<Account> accounts = bank.users.get(user);
+        Account rsl = null;
+        for (Account account : accounts) {
+      String req = account.getRequisite();
+            if (req.equals(requisite)) {
+                 rsl = account;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String dеstRequisite, double amount) {
+        boolean rsl = false;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+
+        BankService bank = new BankService();
+        Map<User, List<Account>> map1 = new HashMap<>();
+        User user = new User("3434", "Petr Arsentev");
+        List<Account> accounts = new ArrayList<>();
+        bank.addUser(user);
+        System.out.println(bank.findByPassport("3434"));
+        Account account1 = new Account("1223", 1000);
+        Account account2 = new Account("2345", 2000);
+        accounts.add(account1);
+        accounts.add(account2);
+        System.out.println(accounts.size());
+        map1.put(user, accounts);
+        System.out.println("00user " + bank.findByPassport("3434"));
+        boolean value = false;
+        List<Account> list = map1.get(user);
+        for (Account account : list) {
+            System.out.println(account);
+            System.out.println(account.getRequisite());
+            value = account.getRequisite().equals("2345");
+                    }
+        System.out.println(value);
+        System.out.println(bank.findByRequisite("3434", "2345"));
+    }
+}
