0639f63a1cf014baf6ec953924e34d8a5ebea3db
000SergeyMayer000 <samajor@mail.ru>
2020-01-12 00:48:42 +0300

    3. Банковские переводы. [#10038]                                               replace BankService.java & BankServiceTest.java add Test.java

--- chapter_002/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_002/src/main/java/ru/job4j/bank/BankService.java
@@ -10,10 +10,11 @@ public class BankService {
     private Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
-        List<Account> accounts = new ArrayList<>();
+
         if (users.containsKey(user)) {
             System.out.println("Such a client exists");
         } else {
+            List<Account> accounts = new ArrayList<>();
             users.put(user, accounts);
         }
     }
@@ -31,13 +32,11 @@ public class BankService {
     }
 
     public User findByPassport(String passport) {
-          User rsl = null;
+        User rsl = null;
         for (User user : users.keySet()) {
             if (user.getPassport().equals(passport)) {
                 rsl = user;
                 break;
-            } else {
-                System.out.println("There is no such client");
             }
         }
         return rsl;
@@ -58,36 +57,16 @@ public class BankService {
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
-                                 String destPassport, String dеstRequisite, double amount) {
+                                 String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount != null && srcAccount.getBalance() >= amount) {
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            rsl = true;
+        }
         return rsl;
     }
 
-
-
-    public static void main(String[] args) {
-
-        BankService bank = new BankService();
-        Map<User, List<Account>> map1 = new HashMap<>();
-        User user = new User("3434", "Petr Arsentev");
-        List<Account> accounts = new ArrayList<>();
-        bank.addUser(user);
-        System.out.println(bank.findByPassport("3434"));
-        Account account1 = new Account("1223", 1000);
-        Account account2 = new Account("2345", 2000);
-        accounts.add(account1);
-        accounts.add(account2);
-        System.out.println(accounts.size());
-        map1.put(user, accounts);
-        System.out.println("00user " + bank.findByPassport("3434")); //00user User{passport='3434', username='Petr Arsentev'}
-        boolean value = false;
-        List<Account> list = map1.get(user);
-        for (Account account : list) {
-            System.out.println(account);
-            System.out.println(account.getRequisite());
-            value = account.getRequisite().equals("2345");
-        }
-        System.out.println(value); //true
-        System.out.println(bank.findByRequisite("3434", "2345")); //null
-    }
 }
