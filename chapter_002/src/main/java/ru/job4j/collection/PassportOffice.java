e080e7d2f1b4a503d84814a2131953259ff79076
000SergeyMayer000 <samajor@mail.ru>
2020-02-03 10:07:47 +0300

    raplace PassportOffice. add

--- chapter_002/src/main/java/ru/job4j/collection/PassportOffice.java
+++ chapter_002/src/main/java/ru/job4j/collection/PassportOffice.java
@@ -5,21 +5,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PassportOffice {
-    private Map<String, Citizen> citizens = new HashMap<>();
+     Map<String, Citizen> citizens = new HashMap<>();
 
     /**
      * метод проверяет есть ли такой "гражданин" в карте ( возвращает false) и если нет, то добавляет его в карту
      * @param citizen - проверяемый "гражданин"
      * @return если есть гражданин - true, если нет - false
      */
+
     public boolean add(Citizen citizen) {
-        boolean rsl = true;
-        if (!citizens.containsKey(citizen.getPassport())) {
-            citizens.put(citizen.getPassport(), citizen);
-            rsl = false;
-        }
-        return rsl;
+
+        return citizens.put(citizen.getPassport(), citizen) == null; //метод put возращает предыдущее значение,
+        // связанное с ключом , или ноль, если не было сопоставления для ключа .
+        // ( Нулевой возврат также может указывать, что карта ранее ассоциировала нуль с ключом .)
+
     }
+//    public boolean add(Citizen citizen) {
+//        boolean rsl = true;
+//        if (!citizens.containsKey(citizen.getPassport())) {
+//            citizens.put(citizen.getPassport(), citizen);
+//            rsl = false;
+//        }
+//        return rsl;
+//    }
 
     public Citizen get(String passport) {
         return citizens.get(passport);
